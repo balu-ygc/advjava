8183617185533f2a90ebc683f0c4def73844114d
Admin <balu.n408@gmail.com>
2022-02-09 14:40:43 +0530

    new dire

---
+++ advjava/CPathDemo/MianApp.java
@@ -0,0 +1,8 @@
+import com.nit.basics.WishMessageGenerator;
+ public class MainApp {
+	public static void main(String[] args) {
+		WishMessageGenerator gen= new WishMessageGenerator();
+		System.out.println("Wish message is :" +gen.sayHello("raja"));
+
+	}
+}
